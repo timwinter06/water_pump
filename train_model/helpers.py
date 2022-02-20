56f25001494a384f0cb979f0a2ce8f93cd0aa4c3
Winter <TW01067@noa.nl.rsg>
2022-02-20 14:15:19 +0100

    dev of train_model.py file

---
+++ train_model/helpers.py
@@ -0,0 +1,64 @@
+""" Helper functions for the train_model.py script. Functions included are:
+ * feature_engineer
+ * preprocess
+ * finalize_data
+
+"""
+import pandas as pd
+
+
+def feature_engineer(x: pd.DataFrame, replace_low_freq: list):
+    """
+    Function where you perform any feature engineering. The following steps are performed:
+    * Fill NaN with 'unknown'
+    * Year and month column are created from 'date_recorded' column
+    * Year column has the minimum year subtracted from it.
+    * Replace low freq categories with "other", for columns in 'replace_low_freq' column.
+    :param pd.DataFrame x: dataframe with features.
+    :param list replace_low_freq: List of columns you want to replace low freq. categories of.
+    :return pd.DataFrame x: dataframe with feature engineered features.
+    """
+    x = x.fillna('unknown')
+    x['date_recorded'] = pd.to_datetime(x.date_recorded)
+    x['year'] = x['date_recorded'].dt.year
+    x['month'] = x['date_recorded'].dt.month
+
+    for col in replace_low_freq:
+        x.loc[x[col].value_counts()[x[col]].values < 20, col] = "other"
+    return x
+
+
+def preprocess(x: pd.DataFrame, type_dict: dict, drop_these: list):
+    """
+    Function where you perform any preprocessing. The following steps are performed:
+    * Drop any duplicates
+    * Make sure some features you select are the correct type
+    * Remove unrealistic values
+    * Fill NaN with 'unknown'
+    * Drop columns you do not want to include.
+    :param pd.DataFrame x: dataframe with features
+    :param dict type_dict: dictionary with types you want features to be in.
+    :param list drop_these: list with columns you want to drop.
+    :return pd.DataFrame x: dataframe with preprocessed features.
+    """
+    x = x.drop_duplicates()
+    x = x.astype(dtype=type_dict)
+    x = x[x.longitude != 0]
+    x = x[x.latitude != 0]
+    x = x.drop(drop_these, axis=1)
+    return x
+
+
+def finalize_data(x: pd.DataFrame, y: pd.DataFrame):
+    """
+    Function to make sure the input features and the target match.
+    :param pd.DataFrame x: dataframe with features
+    :param pd.DataFrame y: dataframe with targets
+    :return (pd.DataFrame, pd.Dataframe) (x, y):
+    """
+    y = pd.merge(x, y, how='left', left_on='id', right_on='id')
+    y = y['status_group']
+    x = x.drop('id', axis=1)
+    return x, y
+
+
---
+++ train_model/train_model.py
@@ -0,0 +1,101 @@
+""" Script to train model. The main steps are:
+* load in feature data and labels.
+* perform processing on data ( using functions from the helpers.py file)
+* Train model
+* Save model and parameters.
+"""
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import category_encoders as ce
+import lightgbm as lgb
+from sklearn.metrics import classification_report
+from helpers import feature_engineer, preprocess, finalize_data
+import pickle
+
+# Model and encode must be set when running script
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", help="model to train", type=str, choices=['lightgbm'])
+parser.add_argument("--encode", help="type of encoder to use", type=str, choices=['ordinal', 'one-hot'])
+args = parser.parse_args()
+
+# Load data
+X_original = pd.read_csv('../water_pump_set.csv')
+y_original = pd.read_csv('../water_pump_labels.csv')
+
+# Define some variables
+# These columns will be checked for low occuring categories. These categories will be replaced by 'other'.
+replace_low_freq = ['funder', 'installer', 'ward']
+# Dictionary to make sure certain columns are the type you want.
+type_dict = {
+    'amount_tsh': 'float64',
+    'gps_height': 'float64',
+    'longitude': 'float64',
+    'latitude': 'float64',
+    'public_meeting': 'str',
+    'permit': 'str',
+    'region_code': 'str',
+    'district_code': 'str',
+    'month': 'str'
+}
+# Columns to be dropped.
+drop_these = [
+    'construction_year', 'wpt_name', 'num_private',
+    'subvillage', 'region', 'scheme_name',
+    'extraction_type', 'extraction_type_group','management',
+    'payment', 'water_quality', 'quantity',
+    'source', 'waterpoint_type', 'population',
+    'recorded_by', 'date_recorded',
+]
+
+# Perform feature engineering and preprocessing
+X = feature_engineer(X_original, replace_low_freq)
+X = preprocess(X, type_dict, drop_these)
+X, y = finalize_data(X, y_original)
+
+# Map target to numeric value
+target_map = {'functional': 0, 'non functional': 1, 'functional needs repair': 2}
+y = y.map(lambda x: target_map[x])
+
+# Create train test-split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Encode input data
+if args.encode == 'ordinal':
+    encoder = ce.OrdinalEncoder()
+elif args.encode == 'one-hot':
+    encoder = ce.OneHotEncoder()
+
+X_train = encoder.fit_transform(X_train)
+X_test = encoder.transform(X_test)
+
+# Save encoder
+outfile = open(f'{args.encode}_encoder', 'wb')
+pickle.dump(encoder, outfile)
+outfile.close()
+
+# Train model
+if args.model == 'lightgbm':
+    clf = lgb.LGBMClassifier()
+
+# Maybe hyper-tune here?
+clf.fit(X_train, y_train)
+
+# Get metrics
+y_pred = clf.predict(X_test)
+print(classification_report(y_test, y_pred, target_names=target_map.keys()))
+
+# Save model
+if args.model == 'lightgbm':
+    clf.booster_.save_model(f'trained_{args.model}.txt')
+    # bst = lgb.Booster(model_file='mode.txt')
+
+
+
+
+
+
+
+
+
